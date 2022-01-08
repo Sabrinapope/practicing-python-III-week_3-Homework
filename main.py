4ea9e2f4f162e85b07a5bfed07e9e7ac4e17c8d9
Sabrinapope.md <40004402+Sabrinapope@users.noreply.github.com>
2022-01-07 22:54:49 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Multiplication_Table")
+for i in range(0, 12):
+  for j in range (1, 11):
+        print('{0} * {1} = {2}'.format(i,j, i*j))
+        print('============')
+
+
+
+def highest_number():
+   print("0, 9")
+
+highest_number()
+
+
+
+
+def numbers_months():
+   print("jan1, feb2, mar3")
+    
+numbers_months()    
